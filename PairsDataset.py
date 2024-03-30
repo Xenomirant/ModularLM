d0ec4a84d59f9eb8aae47c53ea0f5404eb649949
Xenomirant <xenomirant@gmail.com>
2024-03-30 23:08:51 +0300

    add: pretraining

---
+++ PairsDataset.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import transformers
+import torch
+
+class PairsDataset(torch.utils.data.Dataset):
+    def __init__(self,
+                 tokenizer: transformers.AutoTokenizer,
+                 path: str=None,
+                 data: pd.DataFrame=None,
+                 filter_same: bool=True,
+                 SEQ_LEN: int=64):
+        if data is None:
+            data = pd.read_csv(path, index_col=0)
+        self.dataset = data[(data.was_changed) | (not filter_same)].reset_index(drop=True)
+        self.tokenizer = tokenizer
+        self.SEQ_LEN = SEQ_LEN
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx: list[int]):
+        text1 = self.tokenizer(self.dataset.loc[idx]['base'],
+                     padding='max_length',
+                     truncation=True,
+                     max_length=self.SEQ_LEN)
+
+        text2 = self.tokenizer(self.dataset.loc[idx]['polypers'],
+                     padding='max_length',
+                     truncation=True,
+                     max_length=self.SEQ_LEN)
+
+        return text1, text2
+
